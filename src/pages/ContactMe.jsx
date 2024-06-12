6983a5cf4688892013bbee416ba8338e0218e2f3
Monisha Tarkar <monishatarkar1625@gmail.com>
2024-06-12 11:54:01 +0530

    update footer text

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -3,9 +3,8 @@ const ContactMe = () => {
     <div id='contactMe' className='flex flex-col items-center my-10'>
       <span className='text-[36px]'>Get in touch</span>
       <span className='text-gray-500 px-20 pt-0 pb-8 flex flex-wrap text-center'>
-        Feel free to reach out to me if you&apos;re looking for a developer,{' '}
-        <br />
-        have a query, or simply want to connect.
+        Feel free to reach out for development inquiries, <br />
+        questions, or simply to connect.
       </span>
       <span className='font-bold text-xl'>monishatarkar1625@gmail.com</span>
       <span>+91 8073304139</span>
